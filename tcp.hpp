1abc8e6236a19d34c8bbb3fbca2cfbc7cc69c985
Ilichev Danila <you-may@mail.ru>
2023-10-11 02:50:23 +0300

    Initial commit

---
+++ tcp.hpp
@@ -0,0 +1,23 @@
+#ifndef DILICHEV_TCP_HPP
+#define DILICHEV_TCP_HPP
+
+namespace dilichev
+{
+
+namespace tcp
+{
+
+//Classes' declarations
+class Connection;
+class ConnectionPool;
+class Server;
+class Selector;
+
+
+
+}
+
+}
+
+
+#endif
